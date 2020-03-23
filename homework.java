8e6c4d7ccd9981cc38de69ba2c860debcac1ac78
minhthang1204 <61902004+minhthang1204@users.noreply.github.com>
2020-03-23 21:43:20 +0700

    Create homework.java

---
+++ homework.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btap04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Thang dep trai
+ */
+public class Btap04 {
+
+    /**
+     * @param args the command line arguments
+     * @return 
+     */
+    public static int main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //        System.out.print("nhập vào m: ");
+        //        int m = sc.nextInt();
+        //        System.out.print("nhâp vào n: ");
+        //        int n = sc.nextInt();
+        //        System.out.print("nhập vào a: ");
+        //        int a = sc.nextInt();
+        //        System.out.print("nhập vào b: ");
+        //        int b = sc.nextInt();
+        //        int[][] arr = new int[m][n];
+        //        int[][] arr1 = new int[a][b];
+        //        int c[][] = new int[m][b];
+        //
+        //        for (int i = 0; i < arr.length; i++) {
+        //            for (int j = 0; i < arr.length; j++) {
+        //                System.out.println("nhap phan tu thứ [" + i + "," + j + "]");
+        //                arr[i][j] = sc.nextInt();
+        //            }
+        //        }
+        //        for (int i = 0; i < arr1.length; i++) {
+        //            for (int j = 0; i < arr1.length; i++) {
+        //                System.out.println("nhap phan tu thứ [" + i + "," + j + "]");
+        //                arr1[i][j] = sc.nextInt();
+        //            }
+        //        }
+        //
+        //        for (int i = 0; i < m; i++) {
+        //            for (int j = 0; j < b; j++) {
+        //                if (a == m && b == n) {
+        //                    c[i][j] = arr[i][j] + arr1[i][j];
+        //
+        //                    System.out.println(c[i][j] + "");
+        //                } else {
+        //                    System.out.println("KHÔNG CỘNG ĐƯỢC");
+        //                }
+        //            }
+        //        }
+        //    }
+        //ex2
+        //        System.out.print("nhap vao so a: ");
+        //        int a = sc.nextInt();
+        //        System.out.print("nhap vao so b: ");
+        //        int b = sc.nextInt();
+        //        if ((a * b) % 3 == 0 && (a * b) % 5 == 0) {
+        //            System.out.println("true");
+        //
+        //        } else {
+        //            System.out.println("false");
+        //        }
+        //    }
+        //EX3
+        System.out.print("nhập vào kích thước mảng: ");
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < A.length; i++) {
+            System.out.println("nhập vào phần tử thứ" + i + ":");
+            A[n] = sc.nextInt();
+
+            if (A[i] < A[i + 1]) {
+                return 1;
+            }
+            if (A[i] > A[i + 1]) {
+                return -1;
+            }
+            else 
+                return 0;
+        }
+        return 0;
+    }
